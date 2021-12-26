8f2fce969e0d7077871931633d904e83217ee181
jaeyoon <audwodbs492@gmail.com>
2021-12-27 00:44:33 +0900

    리드미 추가 작성, hello 컨트랙트 cpp파일 코드 수정

--- README.md
+++ README.md
@@ -9,3 +9,13 @@
 ## 친절한 트러블 슈팅 - 진행중
 
 - https://wandering-trouser-2c4.notion.site/Eosio-063959904b5246dca45353fff8626a21
+
+# 변경해야 할 부분
+
+- vac패턴을 적용해 eosjsexam의 폴더구조 및 리팩토링
+
+### 2021-12-26
+
+## 친절한 설명서 - 진행중
+
+- Hello World Contracts 작성
--- hello/hello.cpp
+++ hello/hello.cpp
@@ -5,6 +5,6 @@ class [[eosio::contract]] hello : public eosio::contract {
 	    using eosio::contract::contract;
         [[eosio::action]] void hi( eosio::name user ) {
             require_auth( user );
-		    print( "Hello, ", user);
+		    print( "Hello, ", eosio::name{user});
 	    }
 };
